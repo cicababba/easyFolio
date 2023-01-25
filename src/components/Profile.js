3a10aa713e6004b166e68628bea259fcfbf88335
cicababba <cicababba1@gmail.com>
2023-01-25 15:06:56 +0100

    updated style for profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,5 @@
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
 const Profile = ({profileData}) => {
     const {
         profilePictureUrl,
@@ -12,12 +14,17 @@ const Profile = ({profileData}) => {
     } = profileData
 
     return <div className="hero min-h-screen"
-                style={{backgroundImage: `url("${profilePictureUrl}")`}}>
-        <div className="hero-overlay bg-opacity-60"></div>
+                style={{backgroundImage: `url("https://placeimg.com/2000/2000/tech")`}}>
+        <div className="hero-overlay bg-opacity-80"></div>
         <div className="hero-content text-center text-neutral-content">
             <div className="max-w-md">
+                <div className="avatar">
+                    <div className="w-36 lg:w-80 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={profilePictureUrl} alt="Profile"/>
+                    </div>
+                </div>
                 <h1 className="text-5xl font-bold">{name}</h1>
-                <h1 className="text-3xl font-bold">{companyName}</h1>
+                {companyName && <h1 className="text-3xl font-bold">{companyName}</h1>}
                 <h1 className="mb-5 text-xl font-bold">{jobTitle}</h1>
                 <div className="divider"/>
                 <p className="mb-5">{bio}</p>
@@ -29,6 +36,10 @@ const Profile = ({profileData}) => {
                 </a>
                 <p className="text-xs">{phoneNumber}</p>
                 <a target="_blank" href={linkedinUrl} className="text-xs" rel="noreferrer">LinkedIn</a>
+                <div className="divider"/>
+                <div className="mt-10 hover:animate-bounce">
+                    <p className="uppercase">Scroll down for more</p>
+                    <ArrowDropDownIcon/></div>
             </div>
         </div>
     </div>
